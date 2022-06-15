30f70a9f14aac6b0167af10b220fa7efa31589fb
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-16 02:21:44 +0700

    Add files via upload

---
+++ plugins/orgy.js
@@ -0,0 +1,26 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://api-reysekha.herokuapp.com/api/nsfw/orgy?apikey=APIKEY`
+  conn.sendFile(m.chat, res, 'orgy.jpg', `wangy wangy wangy`, m, false)
+}
+handler.help = ['orgy'].map(v => v + ' ')
+handler.tags = ['nsfw']
+
+handler.command = /^(orgy)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
+
