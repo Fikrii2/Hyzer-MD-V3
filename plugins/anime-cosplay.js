226c41d208b604e843a12cf74ca3152fcc846511
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-16 01:06:45 +0700

    Update anime-cosplay.js

--- plugins/anime-cosplay.js
+++ plugins/anime-cosplay.js
@@ -1,9 +1,17 @@
+import fetch from 'node-fetch'
+
 let handler = async (m, { conn }) => {
-let hyzer = 'https://api.zacros.my.id/randomimg/cosplay'
-    conn.sendButtonImg(m.chat, hyzer, teksfoto, wm2, 'NEXT', '.cosplay', m)
+	let url = cosplay[Math.floor(Math.random() * cosplay.length)]
+	conn.sendButton(m.chat, 'Uwwu>\\<', wm, url, [['Next','.cosplay']],m)
 }
 handler.help = ['cosplay']
 handler.tags = ['anime']
 handler.command = /^(cosplay)$/i
-
 module.exports = handler
+
+global.cosplay = [
+  "https://telegra.ph/file/15c418d41bc6cf2134dce.jpg",
+  "https://telegra.ph/file/1e4c30083d825f955272a.jpg",
+  "https://telegra.ph/file/48829ed54dd8d92495552.jpg",
+  "https://telegra.ph/file/4a69324d01ae97990682c.jpg"
+  ]
