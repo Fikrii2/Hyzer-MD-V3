18ae43b85301fcbc36c21cbfee4ee66a64730f7c
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-15 21:34:52 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -1,11 +1,10 @@
-
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Dana
-╠➥ *6282361160044*
+╠➥ *081233738677*
 ╠➥ Chat Owner:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+║- wa.me/6281233738677
+╠═〘 𝑴𝒊𝒌𝒖 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
