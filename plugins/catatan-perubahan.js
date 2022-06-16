eaf114270e84aec5dbe9d40c2bb666271eb9fb10
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-17 00:36:56 +0700

    Update catatan-perubahan.js

--- plugins/catatan-perubahan.js
+++ plugins/catatan-perubahan.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Update Terakhir')).buffer(), `
 Catatan Perubahan :
-`.trim(), 'Senin, 16 Mei 2022 : V. 4.05\n\n◇ Penambahan Fitur : Catatan Perubahan\n\n◇ Update Tampilan\n\n◇ Perbaiki Berapa Bug Seperti Wm Stiker, Untuk Gif Masih Emrror \n\n◇ Total Fitur saat ini: 314\n\n© .Fikriixyz', 'Ok Miku', 'Ok')
+`.trim(), 'Kamis, 17 Juni 2022 : V. 2.05\n\n◇ Penambahan Fitur : Catatan Perubahan\n\n◇ Anime \n\n◇ Sound\n\n◇ Nsfw\n\n◇ Update Tampilan\n\n◇ Perbaiki Berapa Bug Seperti Download Anime, Untuk Gif Masih Emrror \n\n◇ Total Fitur saat ini: ${totalf}:\n\n© .Fikriixyz', 'Ok Miku', 'Ok')
 handler.help = ['note']
 handler.tags = ['info']
 handler.command = /^(catatanperubahan|perubahan|notes)$/i
