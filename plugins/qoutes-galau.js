804cf0a3cdf17143b19b05c3033403117a04e14e
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-16 20:59:02 +0700

    Update qoutes-galau.js

--- plugins/qoutes-galau.js
+++ plugins/qoutes-galau.js
@@ -1,11 +1,37 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
-let handler = async (m, { conn }) => {
+let handler = async (m) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
 let anu =`
 ─────〔 *Galau* 〕─────
 
 ${pickRandom(global.galau)}
 `
-conn.reply(m.chat, anu, m) 
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/quotes.jpg') }, 
+           hydratedFooterText: wm,
+             quickReplyButton: {
+               displayText: 'Galau',
+               id: '.galau',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
 handler.help = ['galau']
 handler.tags = ['quotes']
