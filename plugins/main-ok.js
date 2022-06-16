cdb23dc82d08d1dfa4e83d5d534f3bd9ad894631
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-17 00:57:19 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,9 +1,23 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/ccc3652207ba212c1bbe2.png', m, { packname: "sticker by", author: "Elyas" })
+let name = await conn.getName(m.sender)
+  let wm = global.wm
+hyzer = {
+    key : {
+  remoteJid: 'status@broadcast',
+  participant : '0@s.whatsapp.net'
+    },
+  message: { 
+  "extendedTextMessage": {
+  "text": '*Sama-Sama*',
+  "title": wm,
+  'jpegThumbnail': 'https://telegra.ph/file/20509b31b6e05ff999daa.jpg',
+        }
+       } 
+     }
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', hyzer, { packname: "sticker by", author: "hyzer" })
 }
 
-handler.customPrefix = /^(ok)$/i
+handler.customPrefix = /^(Ok|ok)$/i
 handler.command = new RegExp
 
 module.exports = handler
