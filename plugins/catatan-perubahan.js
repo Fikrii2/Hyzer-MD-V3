721fd3cb915060916a62b87b1739cdf06bf51898
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-17 00:43:43 +0700

    Delete catatan-perubahan.js

--- plugins/catatan-perubahan.js
+++
@@ -1,9 +0,0 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Update Terakhir')).buffer(), `
-Catatan Perubahan :
-`.trim(), 'Kamis, 17 Juni 2022 : V. 2.05\n\n◇ Penambahan Fitur : Catatan Perubahan\n\n◇ Anime \n\n◇ Sound\n\n◇ Nsfw\n\n◇ Update Tampilan\n\n◇ Perbaiki Berapa Bug Seperti Download Anime, Untuk Gif Masih Emrror \n\n◇ Total Fitur saat ini: ${totalf}:\n\n© .Fikriixyz', 'Ok Miku', 'Ok')
-handler.help = ['note']
-handler.tags = ['info']
-handler.command = /^(catatanperubahan|perubahan|notes)$/i
-
-module.exports = handler
