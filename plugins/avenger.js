090602b88e4589b62420762e1eab6173afa60b05
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-17 01:22:12 +0700

    Add files via upload

---
+++ plugins/avenger.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('```Dalam Proses...```')
+  let res = await fetch(`https://api-reysekha.herokuapp.com/api/textpro/avengers-logo?apikey=APIKEY&text1=${response[0]}&text2=${response[1]}`)
+  conn.sendFile(m.chat, res, 'nama.jpg', `Nih Mhank`, m, false)
+}
+handler.help = ['avengers'].map(v => v + ' <teks|teks>')
+handler.tags = ['text']
+handler.command = /^(avengers)$/i
+handler.register = false
+
+handler.limit = true
+
+module.exports = handler
