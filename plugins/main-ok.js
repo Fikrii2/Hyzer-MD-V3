2767792a09afa55bf295a38560a723c021e152aa
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-17 01:05:59 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,16 +1,4 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-key : {
-  remoteJid: 'status@broadcast',
-  participant : '0@s.whatsapp.net'
-    },
-  message: { 
-  "extendedTextMessage": {
-  "text": '*Sama-Sama*',
-  "title": wm,
-  'jpegThumbnail': 'https://telegra.ph/file/20509b31b6e05ff999daa.jpg',
-        }
-       } 
-     }
 
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/a39e85e24bae915e93bb5.png', m, { packname: "sticker by", author: "Elyas" })
 }
