99ef36112cda4424e102400cb2826ccc3c533a05
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-16 15:28:59 +0700

    Update anime-megumin.js

--- plugins/anime-megumin.js
+++ plugins/anime-megumin.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-  let res = await fetch('https://api.waifu.pics/sfw/megumin')
+  let res = await fetch('https://api.lolhuman.xyz/api/random/megumin?apikey=Fiktod')
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
