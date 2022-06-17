bbf1a8111f031439118088286505de65209a0eb8
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:07:09 +0700

    Update asupan-ziva.js

--- plugins/asupan-ziva.js
+++ plugins/asupan-ziva.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/ziva?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.korea', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.ziva', m)
 }
 handler.help = ['ziva']
 handler.tags = ['asupan']
