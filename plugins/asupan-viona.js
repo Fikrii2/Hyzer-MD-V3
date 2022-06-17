1f9fa8242b58d76f0ee0ffb2d902c4ac7947101c
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:06:45 +0700

    Update asupan-viona.js

--- plugins/asupan-viona.js
+++ plugins/asupan-viona.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/viona?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.cecan', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.viona', m)
 }
 handler.help = ['viona']
 handler.tags = ['asupan']
