f2b1d3be9c84d995bd1731b3aaaa4a3c2e09a283
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 02:17:19 +0700

    Update asupan-thailand.js

--- plugins/asupan-thailand.js
+++ plugins/asupan-thailand.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/thailand'
+let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/bocil?apikey=i53VliLP'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.thailand', m)
 }
-handler.help = ['thailand']
+handler.help = ['bocil']
 handler.tags = ['asupan']
 handler.command = /^(thailand)$/i
 
