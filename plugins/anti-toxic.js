9b320c39bd753b7946240261e3a9b2f020899344
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-17 16:07:36 +0700

    Delete anti-toxic.js

--- plugins/anti-toxic.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-let stc = fs.readFileSync('./src/uh.mp3')
-conn.fakeReply(m.chat, stc, '*ADA JAGOAN JADI NGERI*')
-}
-handler.customPrefix = /anjing|memek|kontol|ngentod|goblok|goblog/i
-handler.command = new RegExp
-
-module.exports = handler
