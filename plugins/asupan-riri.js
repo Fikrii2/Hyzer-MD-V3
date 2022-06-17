6abeec1d061aa7ea39a8777f3b840a3c7110f5a1
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:10:51 +0700

    Delete asupan-riri.js

--- plugins/asupan-riri.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/riri?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.riri', m)
-}
-handler.help = ['riri']
-handler.tags = ['asupan']
-handler.command = /^(riri)$/i
-
-module.exports = handler
