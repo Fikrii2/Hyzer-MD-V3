c60875c46c39eb1bf3fecb1852882aee1cf27205
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 02:21:38 +0700

    Update asupan-hijaber.js

--- plugins/asupan-hijaber.js
+++ plugins/asupan-hijaber.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/hijaber'
+let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/mama_gina?apikey=i53VliLP'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.hijaber', m)
 }
-handler.help = ['hijaber']
+handler.help = ['mamagina']
 handler.tags = ['asupan']
 handler.command = /^(hijaber)$/i
 
