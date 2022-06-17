8ea71051a09c546afe37392a4918a400f5461303
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:06:02 +0700

    Update asupan-riri.js

--- plugins/asupan-riri.js
+++ plugins/asupan-riri.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/riri?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.vietnam', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.riri', m)
 }
 handler.help = ['riri']
 handler.tags = ['asupan']
