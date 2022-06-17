1cc8154dd41bb95fffecec3fdca78fd9aac6460c
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 02:16:18 +0700

    Update asupan-jepang.js

--- plugins/asupan-jepang.js
+++ plugins/asupan-jepang.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/japan'
+let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/geayubi?apikey=i53VliLP'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.jepang', m)
 }
-handler.help = ['jepang']
+handler.help = ['geayubi']
 handler.tags = ['asupan']
 handler.command = /^(jepang)$/i
 
