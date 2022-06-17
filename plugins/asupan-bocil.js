8c01c81bfe2a9d22e7b45cfcb0ffc3837bbeed17
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:05:10 +0700

    Update asupan-bocil.js

--- plugins/asupan-bocil.js
+++ plugins/asupan-bocil.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/bocil?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.thailand', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.bocil', m)
 }
 handler.help = ['bocil']
 handler.tags = ['asupan']
