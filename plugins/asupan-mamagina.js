20614a0ff53b55d273a8f02bca1a13430628062b
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:11:54 +0700

    Delete asupan-mamagina.js

--- plugins/asupan-mamagina.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/mama_gina?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.hijaber', m)
-}
-handler.help = ['mamagina']
-handler.tags = ['asupan']
-handler.command = /^(mamagina)$/i
-
-module.exports = handler
