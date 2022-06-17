6e62e829efa3e0359c46ff02afb4015922f332b4
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:11:27 +0700

    Delete asupan-chika.js

--- plugins/asupan-chika.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/chika?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.chika', m)
-}
-handler.help = ['chika']
-handler.tags = ['asupan']
-handler.command = /^(chika)$/i
-
-module.exports = handler
