22acb71c9f7d702937b6777b3413840bac7dba69
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:10:32 +0700

    Delete asupan-viona.js

--- plugins/asupan-viona.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/viona?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.viona', m)
-}
-handler.help = ['viona']
-handler.tags = ['asupan']
-handler.command = /^(viona)$/i
-
-module.exports = handler
