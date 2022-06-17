d74182ea3710336753cd26511f0fa4bb878752c0
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:04:41 +0700

    Update asupan-aura.js

--- plugins/asupan-aura.js
+++ plugins/asupan-aura.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/aura?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.china', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.aura', m)
 }
 handler.help = ['aura']
 handler.tags = ['asupan']
