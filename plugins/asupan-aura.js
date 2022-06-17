2b73ba9446bb3c842ad434b1197b0c0ff9ff5fc4
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:12:27 +0700

    Delete asupan-aura.js

--- plugins/asupan-aura.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/aura?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.aura', m)
-}
-handler.help = ['aura']
-handler.tags = ['asupan']
-handler.command = /^(aura)$/i
-
-module.exports = handler
