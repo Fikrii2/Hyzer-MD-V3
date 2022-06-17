509ddfca75d358deac0994ae7042bf8144f05923
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 03:04:14 +0700

    Update asupan-chika.js

--- plugins/asupan-chika.js
+++ plugins/asupan-chika.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/chika?apikey=i53VliLP'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.indonesia', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.chika', m)
 }
 handler.help = ['chika']
 handler.tags = ['asupan']
