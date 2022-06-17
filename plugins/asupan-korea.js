d2f80abed18077a6cfb92c9cca458df61c3b761e
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-18 02:20:38 +0700

    Update asupan-korea.js

--- plugins/asupan-korea.js
+++ plugins/asupan-korea.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/asupan/korea'
+let api-hyzer = 'https://api.zeeoneofc.xyz/api/asupan/ziva?apikey=i53VliLP'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.korea', m)
 }
-handler.help = ['korea']
+handler.help = ['ziva']
 handler.tags = ['asupan']
 handler.command = /^(korea)$/i
 
