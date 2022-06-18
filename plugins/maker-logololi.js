8f3decc9ecd07e52ec9e7322f8ee028899b18203
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-19 00:24:19 +0700

    Update maker-logololi.js

--- plugins/maker-logololi.js
+++ plugins/maker-logololi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('*Wait Ngab*\nProses...')
-  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=ThiPAkps`
+  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=Miku&apikey=sNPZp39j`
   conn.sendFile(m.chat, res, 'lolilogo.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text>')
