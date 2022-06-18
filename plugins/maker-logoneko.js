49e5230e4c7d266935ce9ae8f0641693947a0bba
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-19 00:53:57 +0700

    Update maker-logoneko.js

--- plugins/maker-logoneko.js
+++ plugins/maker-logoneko.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko elyas|ganzz'
+  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko fikrii|ganzz'
   m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=ThiPAkps`
+  let res = `https://caliphapi.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=sNPZp39j`
   conn.sendFile(m.chat, res, 'nekologo.jpg', `Sudah jadi`, m, false)
 }
 handler.help = ['logoneko'].map(v => v + ' <text|text>')
