bf1da8712a173350a93e2f1210f9dcdf7ce7d825
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-19 00:57:51 +0700

    Update maker-sadboylogo.js

--- plugins/maker-sadboylogo.js
+++ plugins/maker-sadboylogo.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
    if (!args[0]) return m.reply('masukkan text1 dan text2\n\nContok: .sadboylogo elyas|ganzz')
    m.reply('*Wait Ngab*\nProses...')
-  let res = `https://melcanz.com/sadboy?nama=${response[0]}&nama2=${response[1]}&apikey=dUtJxxvp`
+  let res = `https://caliphapi.com/api/sadboy?nama=${response[0]}&nama2=${response[1]}&apikey=sNPZp39j`
   conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['sadboylogo'].map(v => v + ' <text|text>')
