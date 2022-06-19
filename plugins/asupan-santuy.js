8da46594746863021db076731eb6df33ae97f88f
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-19 13:31:00 +0700

    Update asupan-santuy.js

--- plugins/asupan-santuy.js
+++ plugins/asupan-santuy.js
@@ -1,4 +1,5 @@
 let handler = async(m,{text, conn}) => {
+m.reply('*[ ❗ ] Wait,Proses...*')
 let supa = 'https://api.zeeoneofc.xyz/api/asupan/santuy?apikey=i53VliLP'
 conn.sendFile(m.chat, supa, null, 'Nih', m)
 }
