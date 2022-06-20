0286ad5a25d4f2a6263c66248545f95098432764
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-21 02:00:16 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,46 +1,26 @@
-let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╭═══ *〘 PAKET SEWA 〙*
-║ ┅ ๑————————————๑
-║┊ ⌲ Sewa = _5k/grup (1 minggu)_
-║┊ ⌲ Sewa = _15k/grup (1 bulan)_
-║┊ ⌲ Sewa = _20k/grup (2 bulan)_
-║┊ ⌲ Sewa = _35k/grup (Permanent)_ 
-╰═ ┅ ═══════
-╭═══ *〘 PAKET PREMIUM 〙*
-║ ┅ ๑————————————๑
-║┊ ⌲ _5k (Premium 1 Minggu)_
-║┊ ⌲ _10k (Premium 1 Bulan)_
-║┊ ⌲  _25k (Permanent Premium)_
-╰═ ┅ ═══════
-
-╭═══ *〘 𝗣𝗮𝘆𝗺𝗲𝗻𝘁 〙*
-║ ┅ ๑————————————๑
-║┊ ⌲ *DANA* : 081233738677 (diutamakan)
-║┊ ⌲ *GOPAY* : 081233738677
-╰═ ┅ ═══════
-
-༅ _*KEUNTUNGAN SEWABOT*_
-
-✧ *Bot akan masuk gc*
-✧ *Bisa Menyuruh Bot Buka/Tutup Group*
-✧ *Kick Orang*
-✧ *Add Orang*
-✧ *Anti Link On*
-✧ *Bisa Memasukkan Bot Ke dalam Grup*
-
-*[❗]  Jangan Spam Bot*
-
-◪ *Jika Minat Hubungi owner kami*
-◪ *Atau Masuk Group Official Bot*
-_https://chat.whatsapp.com/KpS9eHNALBs3mXXJGtzuZE_
-
-▌│█║▌║▌║║▌║▌║█│▌
-`.trim(), `${wm}`, 'Owner', '#owner', 'Menu', '#menu', m)
+const ftroli = {
+    key : {
+    remoteJid: '6283136505591-1614953337@g.us',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: 'Get Bot', 
+    orderTitle: `Hyzer`,
+    thumbnail: 'https://telegra.ph/file/5ecbec3e82e247671a18e.jpg', 
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
+	conn.reply(m.chat, price1, ftroli)
 }
 
 handler.help = ['sewa', 'sewabot']
 handler.tags = ['info']
-handler.command = /^sewa(bot)?$/i
+handler.command = /^(sewa|sewabot)?$/i
 
 module.exports = handler
