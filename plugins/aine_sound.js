cbdf00b0ba3cd068b11b80b5805e55368d4b85de
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-27 18:33:52 +0700

    Update aine_sound.js

--- plugins/aine_sound.js
+++ plugins/aine_sound.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 let handler = async (m) => {
-let menu = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.52.32-_2_.opus')
+let menu = fs.readFileSync('./mp3/PTT-20220626-WA0491.opus')
 conn.sendFile(m.chat, menu, '', '', m, true)
 }
 
-handler.customPrefix = /^(menu)$/i
+handler.customPrefix = /^(AYANG|ayang|AYANGG|ayangg)$/i
 handler.command = new RegExp
 
-handler.limit = true
+handler.limit = false
 handler.mods = false 
 handler.premium = false
 handler.group = false 
