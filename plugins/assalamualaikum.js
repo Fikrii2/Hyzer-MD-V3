6333f9ecf41c47469c923216266260ddcac1b69d
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-27 17:53:18 +0700

    Update assalamualaikum.js

--- plugins/assalamualaikum.js
+++ plugins/assalamualaikum.js
@@ -1,6 +1,6 @@
 /*let fs = require('fs')
 let handler = async (m) => {
-let assalamualaikum = fs.readFileSync('./mp3/PTT-20220626-WA0030.opus')
+let assalamualaikum = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.46.15.opus')
 conn.sendFile(m.chat, assalamualaikum, '', '', m, true)
 }
 
