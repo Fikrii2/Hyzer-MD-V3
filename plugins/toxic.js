e9aecf6c50753a8b4f674999f482aea1dc0c1ebd
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-27 13:56:10 +0700

    Update and rename ainetoxic.js to toxic.js

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -5,13 +5,13 @@ let gaboleh = fs.readFileSync('./mp3/PTT-20220220-WA0414.opus')
 let ihhomkasar = fs.readFileSync('./mp3/WhatsApp-Audio-2021-06-16-at-14.35.33.opus')
 conn.sendFile(m.chat, ihhomkasar, '', '', m, true)
 setTimeout(() => {
-conn.sendFile(m.chat, gaboleh, '', '', m, true)
+conn.sendFile(m.chat, gaboleh, '', '', m, false)
 }, 1000)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(kontol|memek|jembut|jancok|ngentod)$/i
+handler.customPrefix = /^(Kontol|kontol|Memek|memek|Jembut|jembut|Jancok|jancok|Ngentod|ngentod)$/i
 handler.command = new RegExp
 
 handler.limit = true
