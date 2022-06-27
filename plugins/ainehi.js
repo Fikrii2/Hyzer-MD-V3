c161835d9646eee0a117246ea46040e4cd1e1731
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-27 14:05:08 +0700

    Update ainehi.js

--- plugins/ainehi.js
+++ plugins/ainehi.js
@@ -7,10 +7,10 @@ conn.sendFile(m.chat, hallo, '', '', m, true)
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hi bot|tes|tes|hy|halo|hallo|helo|hello|hy aine|halo aine|hallo aine|helo aine|hello aine)$/i
+handler.customPrefix = /^(.menu|hi|hii|hiii|hi bot|tes|tes|hy|halo|hallo|helo|hello|hy aine|halo aine|hallo aine|helo aine|hello aine)$/i
 handler.command = new RegExp
 
-handler.limit = true
+handler.limit = false
 handler.mods = false 
 handler.premium = false
 handler.group = false
