05367a891374bc16ced7b71bc873bbbc7d8a6fe0
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-28 17:05:12 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./mp3/PTT-20220624-WA0070.opus')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*RECODE SENDIRI*', 'status@broadcast')
+conn.sendFile(m.chat, sc, '', '', m, true)
 }
 handler.customPrefix = /sc|sourcecode|.sc|.sourcecode|script|.script/i
 handler.command = new RegExp
