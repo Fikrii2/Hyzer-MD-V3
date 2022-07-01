1a08ca2cf0f5365df63f37b453aabd45f7af7c0c
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-07-02 05:50:20 +0700

    Create menuvn.js

---
+++ plugins/menuvn.js
@@ -0,0 +1,16 @@
+let fs = require('fs')
+let handler = async (m) => {
+let menu = fs.readFileSync('./mp3/PTT-20220624-WA0070.opus')
+conn.sendFile(m.chat, menu, '', '', m, true)
+}
+
+handler.customPrefix = /^(menu)$/i
+handler.command = /^(menu|help|\?)$/i
+
+handler.limit = true
+handler.mods = false 
+handler.premium = false
+handler.group = false 
+handler.private = false
+
+module.exports = handler
